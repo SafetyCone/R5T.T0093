de1d1769f8b2e9446cc690ec4b2c60759cf051c1
David Coats <david@getsafetycone.com>
2021-12-15 00:12:27 -0800

    Add extension-based functionality.

---
+++ source/R5T.T0093.X001/Code/Bases/Extensions/IPredicateExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using R5T.T0060;
+using R5T.T0093;
+
+
+namespace System
+{
+    public static class IPredicateExtensions
+    {
+        public static Func<T, bool> FilePathIs<T>(this IPredicate _, string filePath)
+            where T : IFilePathed
+        {
+            return filePathed => filePathed.FilePath == filePath;
+        }
+    }
+}
--- source/R5T.T0093.X001/Code/Extensions/IFilePathedExtensions.cs
+++ source/R5T.T0093.X001/Code/Extensions/IFilePathedExtensions.cs
@@ -1,13 +1,31 @@
 using System;
 using System.Collections.Generic;
 
+using R5T.Magyar;
+
 using R5T.T0093;
+
+using Instances = R5T.T0093.X001.Instances;
 
 
 namespace System.Linq
 {
     public static class IFilePathedExtensions
     {
+        public static T[] FindArrayByFilePath<T>(this IEnumerable<T> filePatheds, string filePath)
+            where T : IFilePathed
+        {
+            var output = filePatheds.FindArray(Instances.Predicate.FilePathIs<T>(filePath));
+            return output;
+        }
+
+        public static WasFound<T> FindSingleByFilePath<T>(this IEnumerable<T> filePatheds, string filePath)
+            where T : IFilePathed
+        {
+            var output = filePatheds.FindSingle(Instances.Predicate.FilePathIs<T>(filePath));
+            return output;
+        }
+
         public static Dictionary<string, T> ToDictionaryByFilePath<T>(this IEnumerable<T> items)
             where T : IFilePathed
         {
