12bbd414666be17b13280630296c3e2e2b40e591
David Coats <david@getsafetycone.com>
2022-01-05 12:39:47 -0800

    Add ISelector extensions.

---
+++ source/R5T.T0093.X001/Code/Bases/Extensions/ISelectorExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using R5T.T0060;
+using R5T.T0093;
+
+
+namespace System
+{
+    public static class ISelectorExtensions
+    {
+        public static Func<T, string> SelectFilePath<T>(this ISelector _)
+            where T : IFilePathed
+        {
+            return filedPathed => filedPathed.FilePath;
+        }
+    }
+}
